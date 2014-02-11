d6d0fea4d636692f7e846aa9202a3547372d542d
David A. Greenbaum <zymurgeek@gmail.com>
2014-02-11 13:51:28 -0500

    - Add test instructions to README - Load Event List from local data

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ OnTapWeb
 ========
 
 A browser based version of Barley Legal On Tap.  Provides a list of what's on tap at a Barley Legal Homebrewers event.
+
+To test, cd to the html directory and run
+python -m SimpleHTTPServer
+
+Then, open http://localhost:8000/events.html from a browser.
--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,15 +1,33 @@
 // Global namespace events
 var events = (function() {
-	// Encapsulated variables
+    // Encapsulated variables
 
-	var refreshButton = document.getElementById("refresh-button");
+    var refreshButton = document.getElementById("refresh-button");
+    var eventsList = document.getElementById("events-list");
 
-	// Register event listeners
-    	refreshButton.addEventListener('click', refreshEvents, false);
+    // Register event listeners
+    refreshButton.addEventListener('click', refreshEvents, false);
 
-	function refreshEvents()
-    	{
-		alert("Query server for events from main.js.");
+    function refreshEvents() {
+	alert("Query server for events from main.js.");
+	loadData('http://localhost:8000/data/events.html', eventsList);
+    }
+
+    function loadData(dataUrl, target) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', dataUrl, true);
+	xhr.onreadystatechange = function() {
+  	    if (xhr.readyState == 4) {
+		if((xhr.status >=200 && xhr.status <300) ||
+                   xhr.status===304){
+
+  		    target.innerHTML += xhr.responseText;
+  		} else {
+  		    console.log(xhr.statusText);
+  		}
+  	    }
 	}
+	xhr.send();
+    }
 
 })();
