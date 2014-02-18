fd5f1847779ad55a508fdd08fc654a9b39f5f88c
David A. Greenbaum <zymurgeek@gmail.com>
2014-02-18 15:50:40 -0500

    - Disable refresh button while refreshing - Show timestamp of event data - Format event time to show "mmm dd, yyyy"

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -4,14 +4,18 @@ var events = (function() {
 
     var footerContainer = document.getElementById("ontap-container");
     var refreshButton = document.getElementById("refresh-button");
+    var refreshTimestamp = document.getElementById("refresh-timestamp");
     var eventsList = document.getElementById("events-list");
 
     // Register event listeners
     refreshButton.addEventListener('click', refreshEvents, false);
 
     function refreshEvents() {
+	refreshButton.disabled = true;
 	loadJsonData('barley_legal_events_proxy.php?ble_path=getevent.aspx', 
 		     eventsList);
+	refreshTimestamp.innerHTML = new Date();
+	refreshButton.disabled = false;
     }
 
     function loadJsonData(dataUrl, target) {
@@ -22,7 +26,7 @@ var events = (function() {
   	    if (xhr.readyState == 4) {
 		if((xhr.status >=200 && xhr.status <300) ||
                    xhr.status===304){
-  		    loadJsonResponse(xhr.responseText, target);
+  		    parseAndDisplayJsonResponse(xhr.responseText, target);
   		} else {
 		    footerContainer.innerHTML += '<p class="error">Error getting ' +
 			target.name + ": "+ xhr.statusText + ", code "+
@@ -44,16 +48,32 @@ var events = (function() {
 	return value;
     }
 
-    function loadJsonResponse(jsonResponseText, target) {
+
+    function formatEventDate(dateObject) {
+	var monthAbbrs = new Array("Jan.", "Feb.", "Mar.", "Apr.", "May", 
+				   "June", "July", "Aug.", "Sep.", "Oct.", 
+				   "Nov.", "Dec.");
+	var date = dateObject.getDate();
+	var month = dateObject.getMonth();
+	var year = dateObject.getFullYear();
+	var formattedDate = monthAbbrs[month] + ' ' + date + ', ' + year;
+	return formattedDate;
+    }
+
+
+    function parseAndDisplayJsonResponse(jsonResponseText, target) {
 	target.innerHTML = "";
         var jsonData = JSON.parse(jsonResponseText, dateTimeReviver);
 
         var line = '';
         for(var i= 0; i < jsonData.length; i++) {
 	    if ( jsonData[i].Active == true && jsonData[i].Deleted == false ) {
-		line = '<p><a href="http://misdb.com/barleylegalapp/getbeersforevent.aspx?id=' 
-		    + jsonData[i].ID + '">' + jsonData[i].EventName
-		    + ' ' + jsonData[i].EventDate
+		line = '<p>'
+		    + formatEventDate(jsonData[i].EventDate)
+		    + ' '
+		    + '<a href="http://misdb.com/barleylegalapp/getbeersforevent.aspx?id=' 
+		    + jsonData[i].ID + '">'
+		    + jsonData[i].EventName
 		    + '</a></p>';
 		target.innerHTML += line;
 	    }
